7863af35d7845f11623389e0d7be7953da7d7dc3
Yuqing Ren <yuqing.ren@epfl.ch>
2023-03-07 22:32:28 +0100

    add files

---
+++ graphGenerator/get_permutation_hardware.m
@@ -0,0 +1,51 @@
+% -----------------------------------------------------------------------------------------------------------
+% FUNCTION INFORMATION (c) 2023 Telecommunications Circuits Laboratory, EPFL
+% -----------------------------------------------------------------------------------------------------------
+% name   : get_permutation_hardware
+% descr  : generate codeword permutations in a low-complexity matrix decomposition fashion for hardware
+
+function [graphs_pos, graphs_inv] = get_permutation_hardware(PFG, list_num, N, n)
+
+% parameters
+graphs_pos = zeros(N, list_num);
+graphs_inv = zeros(N, list_num);
+
+% initialization
+data = load('Vset_4096.mat');
+Vset = data.Vset_4096(1:N, 1:n-1); % initial set (we use the recursive property)
+OFG  = 1:1:n;                      % original factor graph               
+
+% algorithm 2: generation of permutations by a matrix decomposition
+for p = 1:1:list_num
+    bit_order = 0:1:N-1;
+
+    % phase 1: generate Vπ
+    for i = 1:1:n
+        s = PFG(p,i); % current column
+        e = OFG(1,i);     % aimed column
+        % update PFG by Vs,e
+        for j = i+1:1:n
+            PFG(p,j) = updateStage(PFG(p,j),s,e);
+        end
+    end
+
+    % phase 2: execute Vπ
+    for i = 1:1:n
+        [bit_order] = subRouting(bit_order, Vset, PFG(p,i), OFG(1,i));
+    end
+
+    graphs_pos(:, p) = bit_order';
+end
+
+% inverse operation
+for p = 1:1:list_num
+    bit_order = 0:1:N-1;
+
+    for i = n:-1:1
+        [bit_order] = subRouting(bit_order, Vset, OFG(1,i), PFG(p,i));
+    end
+
+    graphs_inv(:, p) = bit_order';
+end
+
+end
